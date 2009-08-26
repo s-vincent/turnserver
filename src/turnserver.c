7dc5bfaa9c9e4fb7b52af70e5b76c64d1a50291d
s-vincent <s-vincent@52016ef8-643f-4567-8c35-e81e6bf9b430>
2009-08-26 12:37:33 +0000

    Tag version 0.2.3.

--- src/turnserver.c
+++ src/turnserver.c
@@ -87,7 +87,7 @@
  * \def SOFTWARE_DESCRIPTION
  * \brief Textual description of the server.
  */
-#define SOFTWARE_DESCRIPTION "TurnServer 0.3"
+#define SOFTWARE_DESCRIPTION "TurnServer 0.2.3"
 
 /**
  * \def DEFAULT_CONFIGURATION_FILE
