e42712d5f495cc7d32f46a5e66a5b8f3aa9429d0
s-vincent <s-vincent@52016ef8-643f-4567-8c35-e81e6bf9b430>
2012-02-24 09:13:14 +0000

    Fixes version.

--- src/turnserver.c
+++ src/turnserver.c
@@ -97,7 +97,7 @@
  * \def SOFTWARE_DESCRIPTION
  * \brief Textual description of the server.
  */
-#define SOFTWARE_DESCRIPTION "TurnServer 0.5"
+#define SOFTWARE_DESCRIPTION "TurnServer 0.6"
 
 /**
  * \def DEFAULT_CONFIGURATION_FILE
