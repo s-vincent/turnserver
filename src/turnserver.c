e446eccd403fbe8ae0f93c9405cbecf47cc8bce6
s-vincent <s-vincent@52016ef8-643f-4567-8c35-e81e6bf9b430>
2012-10-17 16:25:41 +0000

    Prepapre 0.7.1 release.

--- src/turnserver.c
+++ src/turnserver.c
@@ -97,7 +97,7 @@
  * \def SOFTWARE_DESCRIPTION
  * \brief Textual description of the server.
  */
-#define SOFTWARE_DESCRIPTION "TurnServer 0.7"
+#define SOFTWARE_DESCRIPTION "TurnServer 0.7.1"
 
 /**
  * \def DEFAULT_CONFIGURATION_FILE
